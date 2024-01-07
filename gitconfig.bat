bc2b4e8f3fc139ab2ea1d87afdc938d65a359a6c
SPACE-AlyeN6378 <alymooltazeem@gmail.com>
2024-01-07 21:09:26 +0600

    Initial commit

--- gitconfig.bat
+++ gitconfig.bat
@@ -14,8 +14,19 @@ REM Configure Git user
 git config --global user.name "%GIT_USERNAME%"
 git config --global user.email "%GIT_EMAIL%"
 
+git add .
+
+REM Commit changes
+set /p COMMIT_MESSAGE=Enter your commit message: 
+git commit -m "%COMMIT_MESSAGE%"
+
+git branch -M main
+
 REM Add GitHub repository as remote origin
 git remote add origin %GITHUB_REPO_URL%
+
+REM Push to GitHub (master branch)
+git push -u origin main
 
 echo Configuration complete!
 pause
