2c8ef92bb95730d30fd00c235d91b0ad10dcc7fd
SPACE-AlyeN6378 <alymooltazeem@gmail.com>
2024-03-28 15:14:21 +0600

    Another standard routers added

--- components/devices/device_creator.py
+++ components/devices/device_creator.py
@@ -17,6 +17,33 @@ def cisco_3600(rtr_id: str, name: str, as_number: int = None) -> Router:
     return router
 
 
+def cisco_7200_v1(rtr_id: str, name: str, as_number: int = None) -> Router:
+    port_numbers = ["0/0", "1/0"]
+    router = Router(
+        router_id=rtr_id,
+        hostname=name,
+        interfaces=[RouterInterface("FastEthernet", port_number) for port_number in port_numbers]
+    )
+    if as_number:
+        router.as_number = as_number
+
+    return router
+
+
+def cisco_7200_v2(rtr_id: str, name: str, as_number: int = None) -> Router:
+    port_numbers = ["0/0", "0/1"]
+    router = Router(
+        router_id=rtr_id,
+        hostname=name,
+        interfaces=[RouterInterface("FastEthernet", port_number) for port_number in port_numbers]
+    )
+    router.add_interface(RouterInterface("GigabitEthernet", "1/0"))
+    if as_number:
+        router.as_number = as_number
+
+    return router
+
+
 def cisco_xr_9000(rtr_id: str, name: str) -> Router:
     port_numbers = Interface.range("0/0/0", range_(0, 7))
     return Router(
