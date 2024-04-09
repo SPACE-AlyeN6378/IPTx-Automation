8fa7a5b23d0e26a1637f6469ac0e5d88510117ad
SPACE-AlyeN6378 <alymooltazeem@gmail.com>
2024-04-09 16:00:52 +0600

    For testing

--- main2.py
+++ main2.py
@@ -4,11 +4,13 @@ from colorama import Fore
 
 
 my_interface = RouterInterface("FastEthernet", "0/0/2", "192.168.12.1/24")
+my_interface.config(mtu=9178)
+
 my_interface.add_sub_if(2024)
 my_interface.add_sub_if(3192)
 my_interface.add_sub_if(1024)
 my_interface.add_sub_if(335)
 
-my_interface.get_sub_if(2024).config(cidr="192.168.12.1/24")
+my_interface.get_sub_if(2024).generate_pseudowire_config("10.255.255.3")
 
 NetworkDevice.print_script(my_interface.generate_config(), color=Fore.CYAN)
