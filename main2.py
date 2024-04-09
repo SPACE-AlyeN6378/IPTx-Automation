b200e3c5efa36cdbbdad53fbdb470098f47b11b4
SPACE-AlyeN6378 <alymooltazeem@gmail.com>
2024-04-09 15:54:54 +0600

    For testing

--- main2.py
+++ main2.py
@@ -1,5 +1,14 @@
-from iptx_utils import print_warning
+from components.interfaces.physical_interfaces.router_interface import RouterInterface
+from components.devices.network_device import NetworkDevice
+from colorama import Fore
 
-print_warning("This is a warning.")
 
-print("Code executed!")
+my_interface = RouterInterface("FastEthernet", "0/0/2", "192.168.12.1/24")
+my_interface.add_sub_if(2024)
+my_interface.add_sub_if(3192)
+my_interface.add_sub_if(1024)
+my_interface.add_sub_if(335)
+
+my_interface.get_sub_if(2024).config(cidr="192.168.12.1/24")
+
+NetworkDevice.print_script(my_interface.generate_config(), color=Fore.CYAN)
